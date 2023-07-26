eba892171ce7496c948fd1427ce134c45f4d83bb
Roman Bykov <Bef93@mail.ru>
2023-07-26 19:12:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2000; // Цена билета
+        int cost = 20; // Стоимость 1 бонуса
+        int bonus = price / cost;
+        System.out.println("Количество бонусов: " + bonus);
+
+    }
+}
